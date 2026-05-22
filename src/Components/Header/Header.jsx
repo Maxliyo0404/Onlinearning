a38be1700d5220efa1f2167bf31634be681aacac
Mahliyo <gasandigital13@gmail.com>
2026-05-22 21:48:36 +0500

    Onlinearning start header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import "./Header.css";
 import React from 'react'
-
+import i18next from "i18next";
 
 function Header() {
   return (
     <div className="header">
         <div className="container">
             <div className="header-wrapper">
-                
+
             </div>
         </div>
     </div>
