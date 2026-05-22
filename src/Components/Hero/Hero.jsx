48fcd70ebd1fff1dd4f0d3c22ec8ef2fb25bd54f
Mahliyo <gasandigital13@gmail.com>
2026-05-22 22:51:31 +0500

    hero start css responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,17 @@ function Hero() {
     <div className="hero">
         <div className="container">
             <div className="hero-wrapper">
-                
+                <div className="hero-info">
+                    <h1 className="">{t("hero.hero-title")}</h1>
+                    <p className="">{t("hero.hero-text")}</p>
+                    <button className="hero-btn">{t("hero.hero-btn")}</button>
+                </div>
+                <div className="hero-cards">
+                    <div className="hero-card">
+                        <div className="hero-icon"></div>
+                        <h4></h4>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
