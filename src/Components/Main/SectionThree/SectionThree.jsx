b0b31526277dcb7b0f4989fd3c96155b6928a410
Mahliyo <gasandigital13@gmail.com>
2026-05-25 22:20:05 +0500

    sectionThree cssclear

--- src/Components/Main/SectionThree/SectionThree.jsx
+++ src/Components/Main/SectionThree/SectionThree.jsx
@@ -1,8 +1,9 @@
+import { useTranslation } from "react-i18next";
 import "./SectionThree.css";
 import React from 'react'
 
 function SectionThree() {
-    const [t, i18n] =
+    const [t, i18n] = useTranslation();
   return (
     <div className="sectionThree">
         <div className="container">
