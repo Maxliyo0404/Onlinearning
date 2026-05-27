e703a45e89b64a2f668917a71299c01d725a8a19
Mahliyo <gasandigital13@gmail.com>
2026-05-27 19:11:55 +0500

    sectionSix

--- src/Components/Main/SectionFive/SectionFive.jsx
+++ src/Components/Main/SectionFive/SectionFive.jsx
@@ -1,7 +1,11 @@
 import { useTranslation } from "react-i18next";
 import "./SectionFive.css";
 import React from 'react'
-
+import { FaRegClock } from "react-icons/fa6";
+import { CiLocationOn } from "react-icons/ci";
+import five_img1 from "./image/five_img1.svg";
+import five_img2 from "./image/five_img2.svg";
+import five_img3 from "./image/five_img3.svg";
 function SectionFive() {
     const [t, i18n] = useTranslation();
   return (
@@ -15,18 +19,56 @@ function SectionFive() {
                 <ul className="five-list">
                     <li className="five-item">
                         <div className="five-imgs">
-                            <img className="five-img" src={} alt="rasm" />
-                            <span className="five-span">  10 APR</span>
+                            <img className="five-img" src={five_img1} alt="rasm" />
+                            <span className="five-span">10 APR</span>
                         </div>
                         <div className="fiveList-titles">
-                            <h3 className="fiveList-title">{t("sectionFive.fiveList-title")}</h3>
+                            <h3 className="fiveList-title">{t("sectionFive.fiveList-title1")}</h3>
                             <div className="fiveList-icons">
                                 <div className="fiveList-icon">
-                                    <div className="five-icon"></div>
+                                    <div className="five-icon"><FaRegClock /></div>
                                     <p className="fiveList-text">{t("sectionFive.fiveList-text1")}</p>
                                 </div>
                                 <div className="fiveList-icon">
-                                    <div className="five-icon"></div>
+                                    <div className="five-icon"><CiLocationOn /></div>
+                                    <p className="fiveList-text">{t("sectionFive.fiveList-text2")}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="five-item">
+                        <div className="five-imgs">
+                            <img className="five-img" src={five_img2} alt="rasm" />
+                            <span className="five-span">10 APR</span>
+                        </div>
+                        <div className="fiveList-titles">
+                            <h3 className="fiveList-title">{t("sectionFive.fiveList-title2")}</h3>
+                            <div className="fiveList-icons">
+                                <div className="fiveList-icon">
+                                    <div className="five-icon"><FaRegClock /></div>
+                                    <p className="fiveList-text">{t("sectionFive.fiveList-text1")}</p>
+                                </div>
+                                <div className="fiveList-icon">
+                                    <div className="five-icon"><CiLocationOn /></div>
+                                    <p className="fiveList-text">{t("sectionFive.fiveList-text2")}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="five-item">
+                        <div className="five-imgs">
+                            <img className="five-img" src={five_img3} alt="rasm" />
+                            <span className="five-span">10 APR</span>
+                        </div>
+                        <div className="fiveList-titles">
+                            <h3 className="fiveList-title">{t("sectionFive.fiveList-title3")}</h3>
+                            <div className="fiveList-icons">
+                                <div className="fiveList-icon">
+                                    <div className="five-icon"><FaRegClock /></div>
+                                    <p className="fiveList-text">{t("sectionFive.fiveList-text1")}</p>
+                                </div>
+                                <div className="fiveList-icon">
+                                    <div className="five-icon"><CiLocationOn /></div>
                                     <p className="fiveList-text">{t("sectionFive.fiveList-text2")}</p>
                                 </div>
                             </div>
